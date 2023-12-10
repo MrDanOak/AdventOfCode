ff5ab39d12728b8119002779aab0485162d6f804
Dan Oak <coderdan@protonmail.com>
2023-12-10 14:19:07 +0000

    Day10 Part 1

--- 2023/AdventOfCode/Solutions/Day10.cs
+++ 2023/AdventOfCode/Solutions/Day10.cs
@@ -32,7 +32,7 @@ namespace AdventOfCode.Solutions
                 _ => []
             };
 
-            public bool ConnectsTo(Pipe otherPipe, Direction? excluding) => 
+            public bool ConnectsTo(Pipe otherPipe, Direction? excluding = null) => 
                 ((IsAnimal || (excluding != Direction.Left && Connectors.Contains(Direction.Left))) && (otherPipe.Connectors.Contains(Direction.Right) || otherPipe.IsAnimal) && X == otherPipe.X + 1 && Y == otherPipe.Y) ||
                 ((IsAnimal || (excluding != Direction.Right && Connectors.Contains(Direction.Right))) && (otherPipe.Connectors.Contains(Direction.Left) || otherPipe.IsAnimal) && X == otherPipe.X - 1 && Y == otherPipe.Y) || 
                 ((IsAnimal || (excluding != Direction.Down && Connectors.Contains(Direction.Down))) && (otherPipe.Connectors.Contains(Direction.Up) || otherPipe.IsAnimal) && Y == otherPipe.Y - 1 && X == otherPipe.X) ||
@@ -70,25 +70,20 @@ namespace AdventOfCode.Solutions
 
         public object Part1() 
         {
-            var startPath = new Path(_pipes.Where(x => x.IsAnimal).ToList());
+            var animal = _pipes.First(x => x.IsAnimal);
+            var startPath = new Path(new List<Pipe>(){ animal });
             var allPaths = new List<Path> { startPath };
             var pathsLoopedRoundToAnimal = new List<Path>();
 
-            while (pathsLoopedRoundToAnimal.Count == 0 || allPaths.Count > 0)
+            while (allPaths.Count(x => x.Pipes.Where(pipe => pipe.ConnectsTo(animal)).Count() == 2) == 0)
             {
                 var pathsToAdd = new List<Path>();
-                var pathsToRemove = new List<Path>();
                 foreach(var path in allPaths) 
                 {
                     var pipe = path.Pipes.Last();
                     var connectingPipes = _pipes
                         .Where(otherPipe => pipe.ConnectsTo(otherPipe, path.CurrentDirection))
                         .ToList();
-
-                    if (connectingPipes.Count == 0)
-                    {
-                        pathsToRemove.Add(path);
-                    }
 
                     for (var i = connectingPipes.Count - 1; i >= 0; i--) 
                     {
@@ -107,10 +102,12 @@ namespace AdventOfCode.Solutions
                         }
                     }
                 }
-                allPaths.RemoveAll(x => pathsToRemove.Contains(x));
                 allPaths.AddRange(pathsToAdd);
             }
-            return Math.Round(allPaths.Min(x => x.Pipes.Count) / 2d, MidpointRounding.AwayFromZero);
+
+            var connectingToAnimals = allPaths.First(x => x.Pipes.Where(pipe => pipe.ConnectsTo(animal)).Count() == 2);
+
+            return Math.Round(connectingToAnimals.Pipes.Count() / 2d, MidpointRounding.AwayFromZero);
         }
 
         public object Part2() => "";
